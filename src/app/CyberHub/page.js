5968ad19d3ac2665c0b9a019f6fa0f7f7a776e96
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-03-28 22:31:31 +0530

    Education updated

---
+++ src/app/CyberHub/page.js
@@ -0,0 +1,9 @@
+import CyberHub from "@/components/CyberHub"
+
+const page = () => {
+  return (
+    <div><CyberHub></CyberHub></div>
+  )
+}
+
+export default page
---
+++ src/components/ReadStories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReadStories = () => {
+  return (
+    <div className='shadow-xl w-72'><img src="" alt="" /></div>
+  )
+}
+
+export default ReadStories
