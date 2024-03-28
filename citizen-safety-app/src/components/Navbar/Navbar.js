128def4ae2a691fd3ee0109d52df85043a127653
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-03-28 12:37:28 +0530

    added sms analysis

--- citizen-safety-app/src/components/Navbar/Navbar.js
+++ citizen-safety-app/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
       <header className="fixed flex w-full justify-between px-10 lg:px-24 py-2 lg:py-8  dark:bg-slate-800 dark:text-white bg-gradient-to-r  from-40% from-slate-800 via-slate-700 to-60% to-slate-800 bg-opacity-50">
         <div>Citizen Safety App</div>
-        <ul className="flex dark:text-slate-400 space-x-4  ">
+        <ul className="flex dark:text-slate-401 space-x-5   text-white ">
           <li className="">
             <Link href="/">Home</Link>
           </li>
@@ -16,12 +16,12 @@ const Navbar = () => {
           <li className="">
             <Link href="/SmsAnalysis">SmsAnalysis</Link>
           </li>
-          <li className="">
+          {/* <li className="">
             <Link href="/FraudDetection">FraudDetection</Link>
-          </li>
-          <li className="">Bitcoin Wallets</li>
+          </li> */}
+          {/* <li className="">Bitcoin Wallets</li> */}
           <li className="">Fraud Reporting</li>
-          <li className="">Privacy Education</li>
+          <li className="">Education</li>
         </ul>
     </header>
   );
